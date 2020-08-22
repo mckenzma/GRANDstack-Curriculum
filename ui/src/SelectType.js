26ff8e27cab4b10fec3e1c1cf43b1ddf5d5d593c
Michael McKenzie <mckenzma@gmail.com>
2020-08-22 16:59:43 -0400

    SelectType (WIP - need to resolve issues with props)

--- ui/src/SelectType.js
+++ ui/src/SelectType.js
@@ -18,17 +18,26 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+// TO FIX - for creating adding a new technique need "_type, _setType"
+// for updating need "props, onUpdate"
 
-// TODO - update to pull labels from Technique
 export default function SelectType({ _type, _setType }) {
+// export default function SelectType( props ) {
   const classes = useStyles();
+  // console.log(props);
+  // const { value, onChange } = props;
   const [type, setType] = useState(_type);
+  // const [type, setType] = useState(value);
+
+
 
   const handleChange = (event) => {
     setType(event.target.value);
     _setType(event.target.value);
+    // onChange(event.target.value);
   };
 
+  // TODO - set this list dynamically
   return (
     <div>
       <FormControl className={classes.formControl}>
